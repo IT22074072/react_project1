02949496169703b9c5024d678c9a544c5bd76eb3
IT22074072 <it22074072@my.sliit.lk>
2024-12-15 09:39:05 +0530

    complete multiple selection

--- frontend/src/components/accordian/index.jsx
+++ frontend/src/components/accordian/index.jsx
@@ -1,35 +1,55 @@
-//single selection
-//multiple selection
-
 import { useState } from "react";
 import data from "./data";
-import './styles.css'
+import "./styles.css";
 
 export default function Accordian() {
   const [selected, setSelected] = useState(null);
+  const [enableMultiSelection, setenableMultiSelection] = useState(false);
+  const [multipleselected, setMultipleSelected] = useState([]);
 
   function handleSingleSelection(currentId) {
-    console.log(currentId);
     setSelected(currentId === selected ? null : currentId);
   }
 
+  function handleMultiSelection(currentId) {
+    const cpyMultiple = [...multipleselected];
+    const indexOfCurrentId = cpyMultiple.indexOf(currentId);
+
+    if (indexOfCurrentId === -1) cpyMultiple.push(currentId);
+    else cpyMultiple.splice(indexOfCurrentId, 1);
+
+    setMultipleSelected(cpyMultiple);
+  }
+
+  console.log(selected, multipleselected)
+
   return (
     <div className="wrapper">
+      <button onClick={() => setenableMultiSelection(!enableMultiSelection)}>
+        {enableMultiSelection ? "Disable multi-selection" : "Enable multi-selection"}
+      </button>
       <div className="accordian">
         {data && data.length > 0 ? (
           data.map((dataItem) => (
-            <div className="item">
+            <div className="item" key={dataItem.id}>
               <div
-                onClick={() => handleSingleSelection(dataItem.id)}
+                onClick={
+                  enableMultiSelection
+                    ? () => handleMultiSelection(dataItem.id)
+                    : () => handleSingleSelection(dataItem.id)
+                }
                 className="title"
               >
                 <h3>{dataItem.question}</h3>
                 <span>+</span>
               </div>
-
-              {selected === dataItem.id ? (
-                <div className="content">{dataItem.answer}</div>
-              ) : null}
+              {enableMultiSelection
+                ? multipleselected.includes(dataItem.id) && (
+                    <div className="content">{dataItem.answer}</div>
+                  )
+                : selected === dataItem.id && (
+                    <div className="content">{dataItem.answer}</div>
+                  )}
             </div>
           ))
         ) : (
